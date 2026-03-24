403bd173bd646d04d171f34f48dd0a0ce9ca4a25
akokits <an.kokits@gmail.com>
2026-03-24 13:06:46 +0100

    Plots for different freqeuncies and establishing ForestML

--- plot.py
+++ plot.py
@@ -1,8 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
+import sys
+from scipy.signal import butter, filtfilt, iirnotch, stft
+from scipy.stats import skew, kurtosis
+import copy
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import StandardScaler
 
-fileNumber = 0
+SPS = 250
+window_size = int(0.5 * SPS) #window, in which blinking is analized
+step_size = int(0.1 * SPS)
+
+#Butterworth filter
+def bandpass_filter(data, low_f, high_f, order = 4):
+    nyq = SPS / 2      #Nyquisit frequency: Frequency until which can reliably be represented
+    lowcut = low_f/nyq #0.5Hz highpass
+    highcut = high_f/nyq #70Hz lowpass
+    b, a = butter(order, [lowcut, highcut], btype='band')
+    return filtfilt(b, a, data)
+
+#optional, maybe not needed: COMMON MODE REJECTION filter or 50Hz filter for european signals
+def notch_filter(data, quality=30):
+    """Entfernt Netzstrom-Artefakte (50 Hz in Europa)"""
+    freq = 50 #50Hz
+    b, a = iirnotch(freq, quality, SPS)
+    return filtfilt(b, a, data)
+
+'''read file'''
+if len(sys.argv) < 2 or not sys.argv[1]:
+    fileNumber = int(input("File number: "))
+else:
+    fileNumber = sys.argv[1]
 
 fileNumber = str(fileNumber).zfill(2)
 
@@ -10,11 +41,171 @@ dataFile = 'EEG-IO/S' + fileNumber + '_data.csv'
 labelsFile = 'EEG-IO/S' + fileNumber + '_labels.csv'
 
 df = pd.read_csv(dataFile, usecols=[0, 1], sep=';')
-blinks = pd.read_csv(labelsFile, skiprows=2, usecols=[0])
+blinks = pd.read_csv(labelsFile)
+corrupted = []
 
-df.plot(x='Time (s)', y='FP1')
+if(blinks.columns[1].strip() == '0'):
+    blinks = pd.read_csv(labelsFile, skiprows=2, names=['Time (s)', 'blink'])
+if(blinks.columns[1].strip() == '1'):
+    val = blinks.iloc[0].values
+    corrupted.append((float(val[0]), float(val[1])))
+    blinks = pd.read_csv(labelsFile, skiprows=2, names=['Time (s)', 'blink'])
+'''read file end'''
+
+#Filter
+df['FP1'] = bandpass_filter(df['FP1'].values, 0.5, 70)
+df['FP1'] = notch_filter(df['FP1'].values)
+
+#brainwaves from Banpassfilter
+delta = bandpass_filter(df['FP1'].values, 0.5, 4)
+theta = bandpass_filter(df['FP1'].values, 4, 8)
+alpha = bandpass_filter(df['FP1'].values, 8, 13)
+beta = bandpass_filter(df['FP1'].values, 13, 30)
+gamma = bandpass_filter(df['FP1'].values, 30, 100)
+
+#df.plot(x='Time (s)', y='FP1')
+
+plt.plot(df['Time (s)'], delta, label='delta')
+plt.plot(df['Time (s)'], theta, label='theta')
+#plt.plot(df['Time (s)'], alpha, label='alpha')
+#plt.plot(df['Time (s)'], beta, label='beta')
+#plt.plot(df['Time (s)'], gamma, label='gamma')
+
+#FFT to extract the sgnals
+N = len(df['FP1']) #normilization
+ft_data = df['FP1'].values
+time = df['Time (s)'].values
+
+#ft_data = np.fft.rfft(ft_data)
+#freq = np.fft.rfftfreq(N, 1/SPS) 
+
+fft_results = []
+time_centers = []
+
+X_features = []
+y_labels = []
+
+
+'''
+for start in range(0, len(x) - window_size, step_size):
+    end = start + window_size
+    window = x[start:end]
+
+    # Zeitbereich des Fensters
+    t_start = time[start]
+    t_end = time[end]
+
+    # Label: Blink im Fenster?
+    label = int(any(
+        (blinks['Time (s)'] >= t_start) &
+        (blinks['Time (s)'] <= t_end) &
+        (blinks['blink'] == 1)
+    ))
+
+    y_labels.append(label)
+'''
+
+def band_power(low, high):
+    mask = (freqs >= low) & (freqs <= high)
+    return np.sum(np.abs(X[mask])**2)
+
+for start in range(0, len(ft_data) - window_size, step_size):
+    end = start + window_size
+    window = ft_data[start:end]
+    window = window * np.hamming(len(window))
+
+    # FFT
+    X = np.fft.rfft(window)
+    freqs = np.fft.rfftfreq(len(window), 1/SPS)
+    
+    # speichern
+    fft_results.append(np.abs(X))
+    
+    # mean value of the window
+    t_center = df['Time (s)'].iloc[start:end].mean()
+    time_centers.append(t_center)
+    
+    t_start = time[start]
+    t_end = time[end-1]
+    
+    # Label: Blink im Fenster?
+    label = int(any(
+        (blinks['Time (s)'] >= t_start) &
+        (blinks['Time (s)'] <= t_end) &
+        (blinks['blink'] == 1)
+    ))
+
+    y_labels.append(label)
+
+    # Features
+    features = [
+        np.mean(window),
+        np.var(window),
+        np.max(window) - np.min(window),  # peak-to-peak
+        skew(window),
+        kurtosis(window),
+
+        band_power(0.5, 4),   # delta
+        band_power(4, 8),     # theta
+        #band_power(8, 13),    # alpha
+        #band_power(13, 30),   # beta
+        #band_power(30, 70),  # gamma
+    ]
+
+    X_features.append(features)
+
+scaler = StandardScaler()
+X_features = scaler.fit_transform(X_features)
+
 for index, row in blinks.iterrows():
-    plt.axvline(x=row['Time (s)'], color='red', linestyle='--', alpha=0.7, label='Blink' if row['Time (s)'] == blinks['Time (s)'].iloc[0] else "")
-    idx = (np.abs(df['Time (s)'] - row['corrupt'])).argmin()
+    if row['blink'] == 0:
+        continue
+    if any(start <= row['Time (s)'] <= end for start, end in corrupted):
+        continue
+    idx = (np.abs(df['Time (s)'] - row['Time (s)'])).argmin() #closest timestamp
+    aligned_time = df['Time (s)'].iloc[idx]
+    plt.axvline(x=aligned_time, linestyle = "--", color='red', alpha=0.5, label='Blink' if index == 0 else "")
+    #idx = (np.abs(df['Time (s)'] - row['Time (s)'])).argmin()
 
+
+plt.grid(True)
+plt.legend()
 plt.show()
+
+fft_results = np.array(fft_results).T  
+
+max_freq = 30  
+mask = freqs <= max_freq
+freqs_limited = freqs[mask]
+fft_limited = fft_results[mask, :]
+
+plt.figure()
+plt.pcolormesh(time_centers, freqs_limited, fft_limited)# shading='gouraud')
+'''
+for index, row in blinks.iterrows():
+    if row['blink'] == 0:
+        continue
+    if any(start <= row['Time (s)'] <= end for start, end in corrupted):
+        continue
+    idx = (np.abs(fft_results['Time (s)'] - row['Time (s)'])).argmin() #closest timestamp
+    aligned_time = fft_results['Time (s)'].iloc[idx]
+    plt.axvline(x=aligned_time, linestyle = "--", color='red', alpha=0.5, label='Blink' if index == 0 else "")
+    #idx = (np.abs(df['Time (s)'] - row['Time (s)'])).argmin()
+'''
+    
+plt.xlabel('Time (s)')
+plt.ylabel('Frequency (Hz)')
+plt.title('Sliding Window FFT (Spectrogram)')
+plt.colorbar(label='Amplitude')
+plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_features, y_labels, test_size=0.2, random_state=42
+)
+
+clf = RandomForestClassifier(n_estimators=100)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print(classification_report(y_test, y_pred))
